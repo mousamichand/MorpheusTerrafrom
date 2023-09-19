71fcb5fbcacaa35c2e86aaf7dfa2e51beae82336
Mousami <39524207+mousamichand@users.noreply.github.com>
2023-09-19 14:52:56 +0100

    Update main.tf

--- Group/main.tf
+++ Group/main.tf
@@ -2,5 +2,5 @@ resource "morpheus_group" "tf_example_group" {
   name      = "<%=customOptions.groupName%>"
   code      = "<%=customOptions.groupCode%>"
   location  = "denver"
-  cloud_ids = [1]
+ # cloud_ids = [1]
 }
