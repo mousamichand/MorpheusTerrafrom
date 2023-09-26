6e4c207bec200b7ea3df03b5a654f69b00ee343b
Mousami <39524207+mousamichand@users.noreply.github.com>
2023-09-26 17:49:14 +0100

    Update main.tf

--- Vmware/createFolder/main.tf
+++ Vmware/createFolder/main.tf
@@ -1,5 +1,6 @@
 module "createFolder" {
-   source = "../../modules/VMware/createFolder"
+  # source = "../../modules/VMware/createFolder"
   # source = "git::https://github.com/gomorpheus/morpheus-automation-examples.git"
+source = "./modules/VMware/createFolder/main.tf"
    folder_name = var.folder_name
 }
