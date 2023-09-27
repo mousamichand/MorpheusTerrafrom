768b5584c53b4f784ea1246b293c508aa73e5e11
Mousami <39524207+mousamichand@users.noreply.github.com>
2023-09-27 15:10:44 +0100

    Update main.tf

--- Vmware/createFolder/main.tf
+++ Vmware/createFolder/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    vsphere = {
-      source = "hashicorp/vsphere"
-      version = ">= 0.14.5"
-    }
-  }
-}
-provider "vsphere" {
-  user           = var.vc_user
-  password       = var.vc_pass
-  vsphere_server = var.vc_server
-  allow_unverified_ssl = true
-}
 module "createFolder" {
   # source = "../../modules/VMware/createFolder"
   # source = "git::https://github.com/gomorpheus/morpheus-automation-examples.git"
