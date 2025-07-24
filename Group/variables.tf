7afd2b67e745b4e45cf63040b39065d67c47e7aa
Mousami <39524207+mousamichand@users.noreply.github.com>
2025-07-24 10:56:35 +0100

    Update variables.tf

--- Group/variables.tf
+++ Group/variables.tf
@@ -1,5 +1,10 @@
-variable "vlack" {
-  description = "Slack webhook URL or token"
+variable "MorpheusUrl" {
+  description = Morpheus URL"
+  type        = string
+  sensitive   = true   # marks the variable as sensitive so it won't show up in logs
+}
+variable "Morpheusaccesstoken" {
+  description = Morpheus access token"
   type        = string
   sensitive   = true   # marks the variable as sensitive so it won't show up in logs
 }
