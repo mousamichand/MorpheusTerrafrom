3b46b113bdbdcb07ce066b8ab1ebdbe5ca0fb5a2
Mousami <39524207+mousamichand@users.noreply.github.com>
2025-07-24 11:04:39 +0100

    Update variables.tf

--- Group/variables.tf
+++ Group/variables.tf
@@ -1,8 +1,3 @@
-variable "MorpheusUrl" {
-  description = Morpheus URL"
-  type        = string
-  sensitive   = true   # marks the variable as sensitive so it won't show up in logs
-}
 variable "Morpheusaccesstoken" {
   description = Morpheus access token"
   type        = string
